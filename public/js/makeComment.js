8b446ed850005f9b94e5dca45da2a23460d4af94
KarLOL82 <klinfeldt@gmail.com>
2022-09-28 21:15:51 -0700

    starting comment functionality

---
+++ public/js/makeComment.js
@@ -0,0 +1,29 @@
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    
+    const title = document.querySelector("#post-form").value.trim();
+    const postText = document.querySelector("#postContent").value.trim();
+  
+    
+    if (title && postText) {
+      const response = await fetch("/api/post/new", {
+        method: "POST",
+        
+        body: JSON.stringify({ title, postText }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+       
+        document.location.replace("/profile");
+      } else {
+        alert("Failed to post new blog.");
+      }
+    }
+  };
+  
+  
+  document
+    .querySelector("#post-element")
+    .addEventListener("submit", newPostFormHandler);
