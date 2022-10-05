af390803ad4ad810ab83540f2b0c4761bbe7c3f3
KarLOL82 <klinfeldt@gmail.com>
2022-10-04 21:15:44 -0700

    more comment shenanigans

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,12 @@
 module.exports = {
   format_date: (date) => {
-      // Format date as MM/DD/YYYY
+      
       return date.toLocaleString();
     },
 
     format_date_short: (date) => {
       const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-      // Format date as MM/DD/YYYY
+      
       return date.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'});
     },
 
