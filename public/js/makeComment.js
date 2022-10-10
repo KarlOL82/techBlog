ef3b3de5c6ae862b2728a230f01db39e7e09aeae
KarLOL82 <klinfeldt@gmail.com>
2022-10-09 17:25:40 -0700

    comments are now working. Need to add delete functionality and change user display

--- public/js/makeComment.js
+++ public/js/makeComment.js
@@ -1,48 +1,50 @@
-function showCommentForm (event) {
-    document.querySelector("#"+event.target.dataset.id).classList.remove("hidden");
-    event.target.classList.add("hidden");
-    console.log(event.target.dataset.id);
 
-};
+window.onload = function() {
 
-
-const CommentFormHandler = async (event) => {
+  function showCommentForm(event) {
     event.preventDefault();
-  
-    
-    const post_id = event.target.getAttribute('data-id');
-    const commentText = document.querySelector(".commentContent").value.trim();
-  
-    console.log(post_id,commentText, "FORM COMMENT")
+    console.log(event.target.dataset.id);
+    // if (document.getElementById("addComment" + event.target.dataset.id)) {
+      document
+        .querySelector("#addComment" + this.getAttribute("data-id"))
+        .classList.remove("hidden");
+      event.target.classList.add("hidden");
+      console.log(event.target.dataset.id);
+    // }
+  }
+
+  const CommentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const post_id = event.target.getAttribute("data-id");
+    const commentText = document.querySelector("#commentContent"+post_id).value.trim();
+
+    console.log(post_id, commentText, "FORM COMMENT");
     if (commentText && post_id) {
       const response = await fetch("/api/comment/new", {
         method: "POST",
-        
-        body: JSON.stringify({commentText, post_id}),
+
+        body: JSON.stringify({ commentText, post_id }),
         headers: { "Content-Type": "application/json" },
       });
-      console.log("comment response from comment file ",response);
+      console.log("comment response from comment file ", response);
       if (response.ok) {
-       
         window.location.reload();
       } else {
         alert("Failed to post new comment.");
       }
     }
   };
-  
-  const commentEvents = document.getElementsByClassName("commentArr")
-  for (i =0; i < commentEvents.length; i++) {
+
+  const commentEvents = document.getElementsByClassName("commentArr");
+  for (i = 0; i < commentEvents.length; i++) {
     commentEvents[i].addEventListener("click", showCommentForm);
-    
-  }console.log(commentEvents);
-    
-    
-  
-  const submitCommentEvents = document.getElementsByClassName("postNewComment")
-  for (j =0; j < submitCommentEvents.length; j++) {
-    submitCommentEvents[j].addEventListener("submit", CommentFormHandler)
-  };
-  
-    
-    
+  }
+  console.log(commentEvents);
+
+  const submitCommentEvents = document.getElementsByClassName("postNewComment");
+  for (j = 0; j < submitCommentEvents.length; j++) {
+    submitCommentEvents[j].addEventListener("submit", CommentFormHandler);
+  }
+
+}
