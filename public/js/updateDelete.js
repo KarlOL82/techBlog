146a8295c40e3e2333b193a771325a93fd84d737
KarLOL82 <klinfeldt@gmail.com>
2022-10-11 21:22:08 -0700

    delete is broken now. Merde.

--- public/js/updateDelete.js
+++ public/js/updateDelete.js
@@ -17,39 +17,40 @@ function delButtonHandler(event) {
   }
 }
 
+
+
+// edit comment
+// const updateComHandler = async (event) => {
+  
+//   console.log("click Edit");
+
+//   if (event.target.hasAttribute("data-id")) {
+//     const id = event.target.getAttribute("data-id");
+//     console.log(id);
+//     const response = await fetch(`/api/comment/${id}`, {
+//       method: "PUT",
+//       body: JSON.stringify({
+//         commentText,
+//       }),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+
+//     if (response.ok) {
+//       document.location.reload();
+//     } else {
+//       alert("Failed to edit comment");
+//     }
+//   }
+// };
+
 const deleteBtnArray = document.getElementsByClassName("delComment");
 for (i = 0; i < commentEvents.length; i++) {
   deleteBtnArray[i].addEventListener("click", delButtonHandler);
 }
 
-const updateComHandler = async (event) => {
-  //   event.preventDefault();
-  console.log("click update");
-
-  if (event.target.hasAttribute("data-id")) {
-    const id = event.target.getAttribute("data-id");
-    console.log(id);
-    const response = await fetch(`/api/comment/${id}`, {
-      method: "PUT",
-      body: JSON.stringify({
-        commentText,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    // What happens if the response is ok?
-    // If the response is ok, that means that the dish was updated successfully.
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert("Failed to edit comment");
-    }
-  }
-};
-
-const updateComment = document.getElementsByClassName("delComment");
-for (i = 0; i < commentEvents.length; i++) {
-  updateComment[i].addEventListener("click", updateComHandler);
-}
+// const updateComment = document.getElementsByClassName("editComment");
+// for (i = 0; i < commentEvents.length; i++) {
+//   updateComment[i].addEventListener("click", updateComHandler);
+// }
